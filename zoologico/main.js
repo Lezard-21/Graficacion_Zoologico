38242d08ba4710a765b2c2396f2bf5ffa37e62fe
David Vega Tepetla <pescador2111@hotmail.com>
2023-09-26 22:30:55 -0600

    se agrego la animacion del leon

--- zoologico/main.js
+++ zoologico/main.js
@@ -2,12 +2,12 @@ import './style.css'
 
 import * as THREE from 'three';
 
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 import { MapControls } from 'three/addons/controls/MapControls.js';
-import * as TWEEN from '@tweenjs/tween.js'
+//import * as TWEEN from '@tweenjs/tween.js'
 
-
+import gsap from 'gsap'
 //import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { InteractionManager } from "three.interactive";
@@ -15,17 +15,20 @@ import { InteractionManager } from "three.interactive";
 const scene = new THREE.Scene();
 //scene.fog = new THREE.FogExp2( 0xcccccc, 0.02 );
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,0.1,1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const render = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
 });
 
-render.setPixelRatio (window.devicePixelRatio);
-render.setSize( window.innerWidth,window.innerHeight);
+render.setPixelRatio(window.devicePixelRatio);
+render.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
+//para que tengan sombras los modelos
+// render.setClearColor(0xA3A3A3)
+// render.shadowMap.enabled = true
 
-camera.position.set( 0, 20, -10 );
+camera.position.set(0, 20, -10);
 
 
 
@@ -35,167 +38,146 @@ const progressBar = document.getElementById('progress-bar')
 
 const LoadingManager = new THREE.LoadingManager()
 
-LoadingManager.onProgress = (url,loaded, total)=>{
+LoadingManager.onProgress = (url, loaded, total) => {
     progressBar.value = (loaded / total) * 100;
 }
 
 const progressBarContainer = document.querySelector('.progress-bar-container')
 
-LoadingManager.onLoad = ()=>{
+LoadingManager.onLoad = () => {
     progressBarContainer.style.display = 'none';
 }
 
-//const geometry = new THREE.TorusGeometry(10,3,16,100);
-//const material = new THREE.MeshStandardMaterial({ color: 0xFF6347});
-//const torus = new THREE.Mesh( geometry,material);
-
-
-// const RingGeometry = new THREE.ConeGeometry(5,10,6,1,3.04,6.2)
-// const material2 = new THREE.MeshStandardMaterial( {color: 0xffff00} );
-// const ring = new THREE.Mesh(RingGeometry,material2)
-
-// const sphereGeometry = new THREE.SphereGeometry(3,32,27,0,6.2,0,6.25)
-// const material3 = new THREE.MeshStandardMaterial({color: 0xffff11})
-// const sphere = new THREE.Mesh(sphereGeometry,material3)
-
-const planeGeometry = new THREE.PlaneGeometry(30,30);
-const planeMaterial = new THREE.MeshStandardMaterial({color:0xFFFFFFFF, side:THREE.DoubleSide})
-const plane = new THREE.Mesh(planeGeometry,planeMaterial)
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xFFFFFFFF, side: THREE.DoubleSide, dithering: true })
+const plane = new THREE.Mesh(planeGeometry, planeMaterial)
 plane.rotation.x = -0.5 * Math.PI
 scene.add(plane)
-
-// sphere.position.set(25,25,25)
-//scene.add(torus,ring,sphere);
-// scene.add(ring,sphere);
+// plane.receiveShadow = true
 
 const loader = new GLTFLoader(LoadingManager);
 let mixer;
 const clock = new THREE.Clock()
 
 const interactionManager = new InteractionManager(
-  render,
-  camera,
-  render.domElement
+    render,
+    camera,
+    render.domElement
 );
 
 let model
-loader.load( 'scene.glb', function ( gltf ) {
+loader.load('scene.glb', function (gltf) {
 
     model = gltf.scene;
-	scene.add( model);
-    // model.userData.id = 'leon';
-
-     // Añade el evento de clic al modelo
-//   model.addEventListener('click', (event) => {
-//     event.stopPropagation();
-//     const cube = event.target;
-//     const coords = { x: camera.position.x, y: camera.position.y };
-//     console.log('evento')
-    
-//     // Crea una instancia de Tween para animar la posición de la cámara
-//     new TWEEN.Tween(coords)
-//       .to({ x: cube.position.x, y: cube.position.y })
-//       .onUpdate(() => {
-//         camera.position.set(coords.x, coords.y, camera.position.z);
-//       })
-//       .start();
-//   });
-
-
-    model.position.set(10,0,10)
+    scene.add(model);
+    model.position.set(10, 0, 10)
     mixer = new THREE.AnimationMixer(model);
     const clips = gltf.animations;
-    const clip = THREE.AnimationClip.findByName(clips,'ArmatureAction');
+    const clip = THREE.AnimationClip.findByName(clips, 'ArmatureAction');
     const action = mixer.clipAction(clip);
     action.play();
 
-    model.traverse((node)=>{
-        if(node.isMesh) console.log(node.id)
+    // model.traverse((node)=>{
+    //     if(node.isMesh){
+    //         node.castShadow = true
+    //     }
+    // })
+
+    model.traverse((node) => {
+        if (node.isMesh) console.log(node.id)
     });
 
-}, undefined, function ( error ) {
+}, undefined, function (error) {
 
-	console.error( error );
+    console.error(error);
 
-} );
+});
 
 render.setAnimationLoop(animate);
 
-const positionLinght = new THREE.PointLight(0xffffff,10000);
-positionLinght.position.set(25,25,25)
+const positionLinght = new THREE.PointLight(0xffffff, 10000);
+positionLinght.position.set(25, 25, 25)
 const ambientLight = new THREE.AmbientLight(0xffffff)
 
 scene.add(positionLinght, ambientLight);
-// positionLinght.castShadow = ture
+
+// positionLinght.castShadow = true
+// positionLinght.shadow.mapSize.width = 1024
+// positionLinght.shadow.mapSize.height = 1024
+// positionLinght.shadow.camera.near = 5
+// positionLinght.shadow.camera.far = 20
+// positionLinght.shadow.focus = 1
 
 const lightHelper = new THREE.PointLightHelper(positionLinght)
-const gridHelper = new THREE.GridHelper(200,50)
-scene.add(lightHelper,gridHelper)
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(lightHelper, gridHelper)
 
 //const controls  = new OrbitControls(camera,render.domElement)
-const controls = new MapControls(camera, render.domElement) 
+const controls = new MapControls(camera, render.domElement)
 
 
 const background = new THREE.TextureLoader().load('butter dog.jpg');
 scene.background = background;
 
 function animate(time) {
-    requestAnimationFrame( animate);
+    requestAnimationFrame(animate);
     //torus.rotation.x += 0.01;
     //torus.rotation.y += 0.005;
     //torus.rotation.z += 0.01;
     //torus.position.y = 0.1;
     //if (mixer)
-    mixer.update(clock.getDelta()); 
+    mixer.update(clock.getDelta());
     interactionManager.update();
-    TWEEN.update(time);
-
-       // ring.position.x += .05;
     controls.update();
     render.render(scene, camera);
 
 }
 //animate();
-const mousePosition =  new THREE.Vector2()
+const mousePosition = new THREE.Vector2()
 
- const rayCaster = new THREE.Raycaster()
+const rayCaster = new THREE.Raycaster()
 
-render.domElement.addEventListener('dblclick',(e)=>{
-  //  mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
-//    mousePosition.y = - (e.clientY / window.innerHeight) * 2 - 1;
-   mousePosition.x = ((e.clientX - render.domElement.getBoundingClientRect().left) / render.domElement.clientWidth) * 2 - 1;
-mousePosition.y = -((e.clientY - render.domElement.getBoundingClientRect().top) / render.domElement.clientHeight) * 2 + 1;
- 
+render.domElement.addEventListener('dblclick', (e) => {
+
+    mousePosition.x = ((e.clientX - render.domElement.getBoundingClientRect().left) / render.domElement.clientWidth) * 2 - 1;
+    mousePosition.y = -((e.clientY - render.domElement.getBoundingClientRect().top) / render.domElement.clientHeight) * 2 + 1;
+
     rayCaster.setFromCamera(mousePosition, camera)
-   const intersects = rayCaster.intersectObjects(model.children,true)
-    // if (intersects.length > 0) {
-    //     console.log("Model clicked.");
-    // }
-   for (let i = 0; i < intersects.length; i++) {
-        if (intersects[i].object.id == 29 | 
-intersects[i].object.id == 31 | 
-intersects[i].object.id == 33 | 
-intersects[i].object.id == 34 | 
-intersects[i].object.id == 32 | 
-intersects[i].object.id == 30 ) {
-            camera.position.set( 10, 10, -10 );
+    const intersects = rayCaster.intersectObjects(model.children, true)
+    for (let i = 0; i < intersects.length; i++) {
+        if (intersects[i].object.id == 29 |
+            intersects[i].object.id == 31 |
+            intersects[i].object.id == 33 |
+            intersects[i].object.id == 34 |
+            intersects[i].object.id == 32 |
+            intersects[i].object.id == 30) {
+            console.log(camera.position)
+            // camera.position.set( 6.2, 5, -6 );
+            const tl = gsap.timeline();
+            tl.to(camera.position, {
+                x: 6.2,
+                y: 5,
+                z: -6,
+                duration: 1.5,
+                onUpdate: () => {
+                    camera.lookAt(1, 0, 0)
+                }
+            })
+            tl.to(camera.position, {
+                x: 15,
+                y: 5,
+                z: -6,
+                duration: 1.5,
+                onUpdate: () => {
+                    camera.lookAt(1, 0, 0)
+                }
+            }, 2);
+            controls.enabled = false
         }
     }
-// if(intersects[i].object.name == 'leon'){
-//              camera.position.set( 10, 10, -10);
-    
-//    }
-// }
-   console.log(intersects)
 });
-// 29 main.js:86:33
-// 31 main.js:86:33
-// 33 main.js:86:33
-// 34 main.js:86:33
-// 32 main.js:86:33
-// 30 main.js:86:33
 
-window.addEventListener('resize', ()=>{
+window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     render.setSize(window.innerWidth / window.innerHeight)
